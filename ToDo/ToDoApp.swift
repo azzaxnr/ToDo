f1841d3ae6c272553c51aa3b969ccd9bec41119a
azza <azza.alnayeli11@gmail.com>
2023-07-25 11:18:13 +0400

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Azza Nayel  on 25/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
